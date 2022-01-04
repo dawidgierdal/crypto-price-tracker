4639bacfb7a50bfbd64a7459463038f4fe7b3f82
Dawid Gierdal <dawid.gierdal@mobilereality.pl>
2022-01-04 13:24:21 +0100

    feat: eslint, tsconfig, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended',
+        'prettier',
+    ],
+    plugins: ['react-hooks'],
+    rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        'react/prop-types': 'off',
+        'no-console': 1,
+        'react-hooks/rules-of-hooks': 'error',
+        'object-shorthand': ['error', 'always'],
+        'no-else-return': 2,
+        'no-useless-catch': 0,
+        'react/no-unescaped-entities': 0,
+        // added below two rules because of https://github.com/typescript-eslint/typescript-eslint/issues/1197#issuecomment-581174968
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/no-empty-interface': [
+            'error',
+            {
+                allowSingleExtends: true,
+            },
+        ],
+        'jsx-a11y/anchor-is-valid': [
+            'error',
+            {
+                components: ['Link'],
+                specialLink: ['hrefLeft', 'hrefRight'],
+                aspects: ['invalidHref', 'preferButton'],
+            },
+        ],
+        'react/display-name': 0,
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/ban-ts-comment': 'off',
+                '@typescript-eslint/ban-ts-ignore': 'off',
+                '@typescript-eslint/no-empty-function': 'off',
+            },
+        },
+    ],
+};
